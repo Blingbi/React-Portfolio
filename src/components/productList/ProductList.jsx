70dccf0673493a06bde0b7c14eab6d26ca355890
Blingbi <61753025+Blingbi@users.noreply.github.com>
2022-06-30 16:50:00 -0400

    Add files via upload

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,24 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Create & improve</h1>
+        <p className="pl-desc">
+          I like to improve and desig websites that are heavily utilized,
+          Great websites that are intuitive and clean is what I strive for.
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
